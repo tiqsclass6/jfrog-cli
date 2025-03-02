d200bb415e861c66f1aedd8a78e63a58d0ac980e
tiqsclass6 <daquietstorm22@gmail.com>
2025-03-02 12:44:11 -0800

    changed multiple files

--- ec2_ami.tf
+++ ec2_ami.tf
@@ -1,8 +1,8 @@
 //1. create the ec2 instance
 resource "aws_instance" "example_instance" {
   ami                         = "ami-06b21ccaeff8cd686" # Specify the base AMI ID
-  instance_type               = "t2.micro"     # Specify the instance type
-  associate_public_ip_address = true           # Adjust as needed
+  instance_type               = "t2.micro"              # Specify the instance type
+  associate_public_ip_address = true                    # Adjust as needed
   subnet_id                   = aws_subnet.public_subnet[0].id
 
 
@@ -21,26 +21,26 @@ resource "aws_ami_from_instance" "example_ami" {
 
 //3. Wait for the AMI then Terminate the ec2 instance
 data "aws_instance" "example_running" {
-    filter {
-        name = "instance-state-name"
-        values = ["running"]
-    }
+  filter {
+    name   = "instance-state-name"
+    values = ["running"]
+  }
 
-    instance_id = aws_instance.example_instance.id
+  instance_id = aws_instance.example_instance.id
 
-    depends_on = [aws_ami_from_instance.example_ami]
+  depends_on = [aws_ami_from_instance.example_ami]
 }
 
 output "instance_id" {
-    value = data.aws_instance.example_running.id
+  value = data.aws_instance.example_running.id
 }
 
 resource "null_resource" "delete_instances" {
-    triggers = {
-        instance_id = "${aws_instance.example_instance.id}"
-    }
-    depends_on = [aws_ami_from_instance.example_ami]
-    provisioner "local-exec" {
-        command = "aws ec2 terminate-instances --instance-ids ${aws_instance.example_instance.id}"
-    }
+  triggers = {
+    instance_id = "${aws_instance.example_instance.id}"
+  }
+  depends_on = [aws_ami_from_instance.example_ami]
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.example_instance.id}"
+  }
 }
